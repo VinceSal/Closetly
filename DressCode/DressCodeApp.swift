72da2ffab426d394ceae073ff6e2ad80e182898d
VincenzoSalzano <vincesal26@gmail.com>
2024-11-04 15:36:55 +0100

    Initial Commit

---
+++ DressCode/DressCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DressCodeApp.swift
+//  DressCode
+//
+//  Created by Vincenzo Salzano on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DressCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
