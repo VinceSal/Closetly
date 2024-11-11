c405c9b7a4335d4b63c05c84e918f5209f8a19f1
VinceSal <126887136+VinceSal@users.noreply.github.com>
2024-11-11 15:56:23 +0100

    FirstPatch
    wardrobeView and Container

--- DressCode/Views/WardrobeView.swift
+++ DressCode/Views/WardrobeView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct WardrobeView: View {
     var body: some View {
-        Text("WardrobeView")
+        NavigationStack {
+            VStack {
+                Text("Wardrobe")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading, -185)
+                    .padding(.top, -42)
+
+                // Pulsante per Shirts
+                NavigationLink(destination: ShirtsView()) {
+                    HStack {
+                        Image(systemName: "tshirt.fill") // Icona della maglietta
+                            .font(.title2)
+                            .foregroundColor(.black)
+
+                        Text("Shirts") // Scritta "Shirts"
+                            .font(.title2)
+                            .foregroundColor(.black)
+
+                        Spacer() // Spazio per fare in modo che l'icona e il testo siano sulla sinistra
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.2)) // Colore di sfondo del bottone
+                    .cornerRadius(10) // Raggio degli angoli per arrotondare il bottone
+                    .shadow(radius: 5) // Ombra per il bottone
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: AddClothesView()) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
