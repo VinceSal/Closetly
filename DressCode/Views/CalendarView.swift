2a7a613bf828ff394c5af698204c9ce8965c859a
ruben-tandon <A00833286@tec.mx>
2024-11-12 14:41:00 +0100

    Added the button to select a day of the calendar, and also added the plus button which opens a modal to select outfits for the day specified (only frontend and UI)

--- DressCode/Views/CalendarView.swift
+++ DressCode/Views/CalendarView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct CalendarView: View {
+    
+    @State var showOutfits: Bool = false
+    @State private var date = Date()
+    
     var body: some View {
-        Text("CalendarView")
+        NavigationStack {
+            VStack {
+                DatePicker(
+                    "Select a date:",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                .padding()
+                
+                Text("\(date.formatted(date: .long, time: .omitted)) outfit:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text("Empty")
+                    .font(.title2)
+                
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showOutfits.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title)
+                    }
+                }
+            }
+            .sheet(isPresented: $showOutfits, content: {
+                CalendarOutfitsView(showOutfits: $showOutfits)
+            })
+            .navigationBarTitle("Calendar")
+        }
     }
 }
 
